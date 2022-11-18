1cb5bdc25c91f29416d60579979139b63e97b924
Sayed-Nur-E-Newaz <miznewaz@gmail.com>
2022-11-19 01:42:28 +0600

    fetch api

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from .models import *
 
 import json
+import datetime
 
 # Create your views here.
 
@@ -17,7 +18,7 @@ def store(request):
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
-		order = {'get_cart_total':0, 'get_cart_items':0}
+		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
 		cartItems = order['get_cart_items']
 
 	products = Product.objects.all()
@@ -37,7 +38,7 @@ def cart(request):
 		order = {'get_cart_total':0, 'get_cart_items':0}
 		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order, 'cartItems':cartItems}
+	context = {'items':items, 'order':order, 'cartItems':cartItems, 'shipping': False}
 	return render(request, 'store/cart.html', context)
 
 
@@ -82,3 +83,33 @@ def updateItem(request):
     print('action', action)
     print('productId', productId)
     return JsonResponse('Item was added', safe=False)
+	
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			state=data['shipping']['state'],
+			zipcode=data['shipping']['zipcode'],
+			)
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
+
